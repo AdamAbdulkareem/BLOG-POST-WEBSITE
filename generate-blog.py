ef9245a890b9d180d8515a8950924c01be5bf639
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-03-04 09:59:12 +0100

    Modify generate-blog.py file

--- generate-blog.py
+++ generate-blog.py
@@ -1,11 +1,20 @@
 import os
 import openai
+import sys
 openai.api_key_path = "api-key.txt"
 openai.api_key = os.getenv("api-key.txt")
+if (len(sys.argv) != 2):
+    print("Error: You have to input the title")
+    exit()
+title = sys.argv[1]
+prompt = "I want to write a blog post about '" + title + \
+    "'. Give a list of 5 sections in a numbered bullet point format about this blog post."
 synopsis = openai.Completion.create(
     model="text-davinci-003",
-    prompt="I want to write a blog post about \"Will AI replace developers\". Give a list of 5 sections in a numbered bullet point format about this blog post.",
+    prompt=prompt,
     max_tokens=3000,
     temperature=0.7
 )
+synopsis = synopsis.choices[0].text
+synopsis = synopsis.strip()
 print(synopsis)
