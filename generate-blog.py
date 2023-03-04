dd041a66425ebb24fa23d0a477c1121ea4f0f04c
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-03-04 10:46:13 +0100

    Modify files

--- generate-blog.py
+++ generate-blog.py
@@ -6,15 +6,37 @@ openai.api_key = os.getenv("api-key.txt")
 if (len(sys.argv) != 2):
     print("Error: You have to input the title")
     exit()
+
 title = sys.argv[1]
+
 prompt = "I want to write a blog post about '" + title + \
     "'. Give a list of 5 sections in a numbered bullet point format about this blog post."
 synopsis = openai.Completion.create(
     model="text-davinci-003",
     prompt=prompt,
-    max_tokens=3000,
+    max_tokens=1000,
     temperature=0.7
 )
 synopsis = synopsis.choices[0].text
 synopsis = synopsis.strip()
 print(synopsis)
+
+
+
+
+lines = synopsis.strip().splitlines()
+for section in lines:
+    prompt = "\nI am writing a blog post with the title '" + title + "'.\n\nThe list of sections of this blog post is the following:\n" + \
+        synopsis + "\n\nWrite the section '" + section + \
+        "' in a detailed and complete way, in 500 words minimum."
+    
+    section_paragraph = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=750,
+        temperature=0.7
+    )
+section_paragraph = section_paragraph.choices[0].text
+section_paragraph = section_paragraph.strip()
+print(section_paragraph)
+exit()
